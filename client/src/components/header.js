f817ae6d320ae5b97c2041250cdd6ce99f81d1c6
NammanShukla <nammanshukla262@gmail.com>
2025-06-24 10:04:51 +0530

    add header

---
+++ client/src/components/header.js
@@ -0,0 +1,12 @@
+import './header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>A Very Simple Sticky Notes</h1>
+    </header>
+  );
+};
+
+export default Header;
+
